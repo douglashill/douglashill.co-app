426894d3a291ba089ed16c92e6930386808ad0f0
Douglas Hill <hello@douglashill.co>
2024-11-23 10:26:13 +0000

    Add basic posting UI

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textToPost = ""
+    @State private var isShowingClearConfirmation = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            TextEditor(text: $textToPost)
+                .font(.body).fontDesign(.monospaced)
+                .navigationTitle(textToPost.count < 10 ? "douglashill.co" : "\(textToPost.count) characters")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Clear") {
+                            isShowingClearConfirmation = true
+                        }
+                        .disabled(textToPost.isEmpty)
+                        .confirmationDialog("Clear Draft", isPresented: $isShowingClearConfirmation) { 
+                            Button("Clear", role: .destructive) {
+                                textToPost = ""
+                            }
+                        }
+                    }
+                    ToolbarItem(placement: .confirmationAction) { 
+                        Button("Post") {
+                            print("Post the thing here")
+                        }
+                    }
+                }
         }
     }
 }
